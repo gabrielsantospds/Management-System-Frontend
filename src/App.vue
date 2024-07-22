2900db59c0be506411443820bc3fbb4908da1d2e
Gabriel <gabrielsantospds@hotmail.com>
2024-07-22 01:05:41 -0300

    set up vuejs project

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <p class="brand">Management <br>System</p>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" aria-current="page" href="#">Translator</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Document</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <!-- <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink> -->
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+.navbar {
+  background-color: #F86011 !important;
+  margin-bottom: 24px
+}
+
+.container-fluid {
+  margin-left: 20px;
+}
+
+.brand,
+.nav-link {
+  color: white;
+}
+
+.nav-link {
+  border-radius: 5px;
+  margin-left: 30px;
+}
+
+.nav-link:hover {
+  background-color: white !important;
+  color: #F86011 !important;
+}
+
+.nav-link:visited {
+  color: white;
+}
+
+.brand {
+  font-size: 17px;
+  font-weight: 500;
+  text-align: center;
+  margin-bottom: 4px;
+}
+</style>
---
+++ src/views/TranslatorView.vue
@@ -0,0 +1,55 @@
+<script>
+</script>
+
+<template>
+  <main class="container">
+    <div class="bg-body-tertiary p-5 rounded">
+      <h1>
+        Management system
+      </h1>
+
+      <p class="lead">
+        This example is a quick exercise to illustrate how the top-aligned navbar works. As you scroll, this navbar remains in its original position and moves with the rest of the page.
+      </p>
+
+      <form action="">
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+          <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="">
+        </div>
+        <div class="mb-3">
+          <label for="exampleFormControlInput1" class="form-label">Email address</label>
+          <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+        </div>
+        <div class="row">
+          <div class="col">
+            <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+            <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+          </div>
+          <div class="col">
+            <label for="exampleFormControlInput1" class="form-label">Full Name</label>
+            <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+          </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  </main>
+</template>
+
+<style>
+
+.rounded {
+  padding: 48px 120px 48px 120px !important;
+}
+
+h1 {
+  color: #333379;
+  text-align: center;
+}
+
+.btn {
+  margin-top: 20px;
+}
+
+</style>
